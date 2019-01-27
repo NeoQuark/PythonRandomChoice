03db7e4843842dcb9a94aab4c199e19a98a833a0
NeoQuark <quentin.courbin@ynov.com>
2019-01-27 10:38:08 +0100

    [Feature] Handled unknown command with else statement

--- randomise.py
+++ randomise.py
@@ -13,7 +13,7 @@ def commands(command):
 	if command == "/q":
 		quit()
 	elif command == "/help":
-		print("\n----------\nYou may insert each element in the list whenever it is prompted to you and press \"Enter\" while leaving the input as blank so the program makes a choice in the list.\n\nHere are some examples of commands you can type :\n- /help		->	Display help\n- /print list	->	Print items of the list\n- /empty list	->	Makes the list empty\n- /q		->	Quit\n\n")
+		print("\n----------\nUsage:\nYou may insert each element in the list whenever it is prompted to you and press \"Enter\" while leaving the input as blank so the program makes a choice in the list.\n\nHere are some examples of commands you can type :\n- /help		->	Display help\n- /print list	->	Print items of the list\n- /empty list	->	Makes the list empty\n- /q		->	Quit\n\n")
 	elif command == "/print list":
 		if len(list) == 0:
 			print("\nYour list is empty\n")
@@ -27,6 +27,8 @@ def commands(command):
 	elif command == "/empty list":
 		del list[:]
 		print("\nThe list is now empty!\n")
+	else:
+		print("\nSorry, unknown command. Check your typing.\n")
 
 def main():
 	while True:
